b7196407c0a7a96b750fd0612b8b09341e43acde
John Lopez <johnllopez616@gmail.com>
2017-01-31 10:20:18 -0800

    Added longFormDate

--- DateDistance/DateDistance.java
+++ DateDistance/DateDistance.java
@@ -65,33 +65,64 @@ public class DateDistance {
         for (int j = 0; j < month1; j++) {
             daysInYear1 = daysInYear1 + daysInMonth(j, year1);
         }
+}
         daysInYear0 = daysInYear0 + day0 + (year0 * 365);
         daysInYear1 = daysInYear1 + day1 + (year1 * 365);
         return Math.abs(daysInYear0 - daysInYear1);
     }
 
+//determine which goes first
+//eitehr recursively or with loop, make the bigger one go down by one
+//keep track of how many days you take away
     /**
      * Returns the day of the week the given date occured on as a String.
      */
     public static String dayOfTheWeek ( long day, long month, long year ) {
-        return ""; // TODO: Finish this method!
+        long dOTW = (day + ((13 * (month + 1)) / 5) + (year % 100) + ((year % 100)/4) + ((year/100)/4) + (5 * (year/100))) % 7;
+        switch((int)dOTW) {
+            case 0: return "Saturday";
+            case 1: return "Sunday";
+            case 2: return "Monday";
+            case 3: return "Tuesday";
+            case 4: return "Wednesday";
+            case 5: return "Thursday";
+            case 6: return "Friday";
+            default: return "Invalid Date!";
+        }
     }
 
     /**
      * Returns the long form of the given date as a String.
      */
+    public static String monthString (long month) {
+        switch((int)month) {
+            case 1: return "January";
+            case 2: return "Feburary";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+            default: return "Invalid Date!";
+        }
+    }
     public static String longformDate ( long day, long month, long year ) {
-        return ""; // TODO: Finish this method!
+            return dayOfTheWeek(day, month, year) + ", " + day + " " + monthString(month) + " " + year; // TODO: Finish this method!
     }
 
     public static void main ( String[] args ) {
-        long day0 = Integer.parseInt(args[0]);
-        long month0 = Integer.parseInt(args[1]);
-        long year0 = Integer.parseInt(args[2]);
-        long day1 = Integer.parseInt(args[3]);
-        long month1 = Integer.parseInt(args[4]);
-        long year1 = Integer.parseInt(args[5]);
-        System.out.println(daysBetween(day0, month0, year0, day1, month1, year1));
+        long day = Integer.parseInt(args[0]);
+        long month = Integer.parseInt(args[1]);
+        long year = Integer.parseInt(args[2]);
+//        long day1 = Integer.parseInt(args[3]);
+//        long month1 = Integer.parseInt(args[4]);
+//        long year1 = Integer.parseInt(args[5]);
+        System.out.println(longformDate(day, month, year));
 
     }
 }
