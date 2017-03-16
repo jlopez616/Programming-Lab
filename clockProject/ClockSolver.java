c13c82ec4ce8ca51f1b23fc7ff1982deb0496693
John Lopez <johnllopez616@gmail.com>
2017-03-16 00:03:47 -0700

    Worked on input

--- clockProject/ClockSolver.java
+++ clockProject/ClockSolver.java
@@ -8,28 +8,38 @@ public class ClockSolver {
     }
 
 
-    public void getAngles() {
-        while (clock.getHours() < 12 /*Clock.MAXHOURS ? */) {
-            clock.tick();
-            System.out.println("Minute: " +  clock.getMinutes());
-            System.out.println("Hour: " +  clock.getHours());
-
-            this.currentAngle = (5.5 * clock.getMinutes());
+    public void getAngles( double degree, double timeSlice) {
+        while (clock.getHours() <= 12 /*Clock.MAXHOURS ? */) {
+            clock.tick(timeSlice);
+        //    System.out.println("Minute: " +  clock.getMinutes());
+        //    System.out.println("Hour: " +  clock.getHours());
+        /*    this.currentAngle = (5.5 * clock.getMinutes());
             if (clock.getSeconds() < 0) {
                 this.currentAngle = this.currentAngle + (clock.getSeconds() * this.ANGLECHANGE);
             }
+        */
+           this.currentAngle = this.currentAngle + (clock.getSeconds() * this.ANGLECHANGE);
             System.out.println(currentAngle);
-        /*    if (this.currentAngle == 180) {
+            if (this.currentAngle == degree) {
                 System.out.println(clock.toString());
             }
-       */
+
         }
     }
 
     public static void main(String args[]) {
-        ClockSolver clockSolver = new ClockSolver();
-        clockSolver.getAngles();
-
+        try {
+            double degree = Double.parseDouble(args[0]);
+            double timeSlice = Double.parseDouble(args[1]);
+            ClockSolver clockSolver = new ClockSolver();
+            if (degree > 0) {
+                clockSolver.getAngles(degree, timeSlice);
+            } else {
+                System.out.println("Usage instructions: java Clocksolver <angle in degrees> <(optional)timeslice in seconds>");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Usage instructions: java Clocksolver <angle in degrees> <(optional)timeslice in seconds>");
+        }
     }
-
 }
