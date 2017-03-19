058fd81224143d6f6d9ac57508153b128910361c
John Lopez <johnllopez616@gmail.com>
2017-03-19 14:36:10 -0700

    Added constructors and toString() (see description)
    *added two basic constructors
    *added a toString() method
    *added a temporary public static void main for testing purposes

--- BigInteger/BigInteger.java
+++ BigInteger/BigInteger.java
@@ -1,3 +1,22 @@
 public class BigInteger {
-    
+
+    public static int numbers;
+    public BigInteger() {
+
+    }
+
+    public BigInteger( String value ) {
+        this.numbers = Integer.parseInt(value);
+    }
+
+    public String toString() {
+        return Integer.toString(this.numbers);
+    }
+
+    public static void main(String args[]) {
+        BigInteger bigInt = new BigInteger(args[0]);
+        System.out.println(bigInt.toString());
+
+    }
+
 }
