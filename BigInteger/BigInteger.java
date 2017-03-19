b84f6dc72e4fa8e924112594378311d9c1df7e4b
John Lopez <johnllopez616@gmail.com>
2017-03-19 15:39:36 -0700

    Got the numbers to go into an array (see description)
    *Commented out the blank constructor (Will ask if I need this in class)
    *Made the constructor construct an array the size necessary for the big integers
    *Fixed the toString() method to properly display each part of the array

--- BigInteger/BigInteger.java
+++ BigInteger/BigInteger.java
@@ -1,16 +1,25 @@
 public class BigInteger {
 
-    public static int numbers;
-    public BigInteger() {
+    public static int[] numbers;
+    /*public BigInteger() {
 
-    }
+    } */
 
     public BigInteger( String value ) {
-        this.numbers = Integer.parseInt(value);
+
+        this.numbers = new int[value.length()];
+        for (int k = 0; k < value.length(); k++) {
+            int intValue = Character.getNumericValue(value.charAt(k));
+            numbers[k] = intValue;
+        }
     }
 
     public String toString() {
-        return Integer.toString(this.numbers);
+        String bigNumber = "";
+        for (int k = 0; k < this.numbers.length; k++) {
+            bigNumber += Integer.toString(numbers[k]);
+        }
+        return bigNumber;
     }
 
     public static void main(String args[]) {
