22765957c65e05608df52ad3148575ff5cb476f5
John Lopez <johnllopez616@gmail.com>
2017-03-21 10:49:51 -0700

    Added a .equals() method

--- BigInteger/BigInteger.java
+++ BigInteger/BigInteger.java
@@ -1,6 +1,7 @@
 public class BigInteger {
 
     public static int[] numbers;
+    public static boolean isPositive = true;
     /*public BigInteger() {
 
     } */
@@ -8,15 +9,37 @@ public class BigInteger {
     public BigInteger( String value ) {
 
         this.numbers = new int[value.length()];
+        int intValue;
         for (int k = 0; k < value.length(); k++) {
-            int intValue = Character.getNumericValue(value.charAt(k));
+            if (value.charAt(k) == '-') {
+                this.isPositive = false;
+            }
+            intValue = Character.getNumericValue(value.charAt(k));
             if (intValue <= 9) {
                 numbers[k] = intValue;
+                //System.out.println(intValue);
             } else {
                 throw new IllegalArgumentException();
             }
 
         }
+    }
+
+    public boolean equals( Object x ) {
+        boolean isSame = true;
+        System.out.println(x.toString().length());
+            System.out.println(this.toString().length());
+        if (x.toString().length() != this.toString().length()) {
+            isSame = false;
+        } else {
+            for (int k = 0; k < x.toString().length(); k++) {
+                if (x.toString().charAt(k) != this.toString().charAt(k)) {
+                    isSame = false;
+                }
+            }
+        }
+
+        return isSame;
     }
 
 
@@ -34,8 +57,16 @@ public class BigInteger {
         if ((numbers[0] == 0) && (this.numbers.length == 1)) {
             bigNumber = "0";
         }
-        return bigNumber;
+
+        if (this.isPositive == false) {
+            return "-" + bigNumber;
+        } else {
+            return bigNumber;
+        }
+
     }
+
+
 
     public static void main(String args[]) {
         BigInteger bigInt = new BigInteger(args[0]);
