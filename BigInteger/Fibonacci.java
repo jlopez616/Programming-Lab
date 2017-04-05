791252deae2bd9bd9d7efc2bb8ec988a1d48505b
John Lopez <johnllopez616@gmail.com>
2017-04-04 22:15:18 -0700

    Several updates (See description)
    1. The program now successfully conducts the fibonacci sequence for any integer larger than zero;.
    2. If the input is a zero, it will return a zero.
    3. If the input is less than zero, ti will give instructions on how to use it.

--- BigInteger/Fibonacci.java
+++ BigInteger/Fibonacci.java
@@ -1,17 +1,28 @@
 public class Fibonacci {
 
     public static BigInteger fib( int val ) {
-        BigInteger fib = new BigInteger(Integer.toString(val));
-        fib = fib.plus( new BigInteger(fib.toString()) );
-        return fib;
+        BigInteger a = new BigInteger("1");
+        BigInteger b = new BigInteger("1");
 
+        for (int i = 3; i <= val; i++) {
+            BigInteger c = a.plus( new BigInteger(b.toString()) );
+            a = b;
+            b = c;
+        }
 
+        if (val == 0) {
+            return BigInteger.ZERO;
+        } else {
+            return fib;
+        }
     }
     public static void main(String[] args) {
-
         int degree = Integer.parseInt(args[0]);
-        System.out.println(fib(degree).toString());
-
+        if (degree < 0) {
+            System.out.println("Usage instructions: 'java Fibonacci <insert integer greater than or equal to zero>'");
+        } else {
+            System.out.println(fib(degree).toString());
+        };
 
     }
 }
