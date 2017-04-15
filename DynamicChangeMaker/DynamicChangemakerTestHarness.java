4bb13324630b556bf51861312de2b0017e038a0b
John Lopez <johnllopez616@gmail.com>
2017-04-15 10:48:31 -0700

    Added tests for Euros, Swiss Francs, and Keckels

--- DynamicChangeMaker/DynamicChangemakerTestHarness.java
+++ DynamicChangeMaker/DynamicChangemakerTestHarness.java
@@ -8,11 +8,9 @@ public class DynamicChangemakerTestHarness {
         successes = 0;
 
         test_USA();
-        // Add more!
-        // A few examples:
-        // test_Euros(); // 1,2,5,10,20,50
-        // test_SwissFrancs(); // 5,10,20,50 (NO 1 CENT COIN!)
-        // test_Keckels(); // 7,3,29,15,53 (Made-up currency with non-standard denominations)
+        test_Euros();
+        test_SwissFrancs();
+        test_Keckels(); // 7,3,29,15,53 (Made-up currency with non-standard denominations)
 
         System.out.println(successes + "/" + attempts + " tests passed.");
     }
@@ -26,6 +24,130 @@ public class DynamicChangemakerTestHarness {
 
     private static void displayFailure() {
         displaySuccessIfTrue(false);
+    }
+
+    public static void test_Keckels() {
+        int[] keckel_Denominations = new int[] { 7, 3, 29, 15, 53 };
+
+        Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(keckel_Denominations, 99);
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_SwissFrancs() {
+        int[] sFDenominations = new int[] { 5, 10, 20, 50 };
+
+        Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(sFDenominations, 95);
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_Euros() {
+        int[] euroDenominations = new int[] { 1, 2, 5, 10, 20, 50 };
+
+        Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(euroDenominations, 99);
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+        try {
+            displaySuccessIfTrue(2 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(5));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+
     }
 
     public static void test_USA() {
