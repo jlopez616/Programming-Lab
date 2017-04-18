22d5b6463bc1ec13ce9d1b63d1272c5738b68030
John Lopez <johnllopez616@gmail.com>
2017-04-18 11:14:06 -0700

    Added several tests (See description)
    Added the following tests:
    *Numerical order for Keckels
    *A single coin for Keckels
    *An impossible return for Swiss Francs
    *5,243 is a value for Euros
    *A numerical order for the US coins
    *5,216 is a value for US coins

--- DynamicChangeMaker/DynamicChangemakerTestHarness.java
+++ DynamicChangeMaker/DynamicChangemakerTestHarness.java
@@ -12,6 +12,8 @@ public class DynamicChangemakerTestHarness {
         test_SwissFrancs();
         test_Keckels(); // 7,3,29,15,53 (Made-up currency with non-standard denominations)
 
+
+
         System.out.println(successes + "/" + attempts + " tests passed.");
     }
 
@@ -27,11 +29,11 @@ public class DynamicChangemakerTestHarness {
     }
 
     public static void test_Keckels() {
-        int[] keckel_Denominations = new int[] { 7, 3, 29, 15, 53 };
+        int[] keckelDenominations = new int[] { 7, 3, 29, 15, 53 };
 
-        Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(keckel_Denominations, 99);
+        Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(keckelDenominations, 99);
 
-        System.out.println("Testing Keckel Denominations...");
+        System.out.println("Testing Keckel Denominations, 99 cents...");
 
         try {
             displaySuccessIfTrue(2 == result.getElement(0));
@@ -67,6 +69,87 @@ public class DynamicChangemakerTestHarness {
             e.printStackTrace();
             displayFailure();
         }
+
+        keckelDenominations = new int[] {3, 7, 15, 29, 53 };
+
+        result = DynamicChangemaker.makeChangeWithDynamicProgramming(keckelDenominations, 99);
+
+        System.out.println("Testing Keckel Denominations, 99 cents (but the denominations are in numerical order)...");
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        keckelDenominations = new int[] { 7, 3, 29, 15, 53 };
+        result = DynamicChangemaker.makeChangeWithDynamicProgramming(keckelDenominations, 29);
+        System.out.println("Testing Keckel Denominations, 29 cents...");
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
     }
 
     public static void test_SwissFrancs() {
@@ -74,7 +157,7 @@ public class DynamicChangemakerTestHarness {
 
         Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(sFDenominations, 95);
 
-        System.out.println("Testing Swiss Franc Denominations...");
+        System.out.println("Testing Swiss Franc Denominations, 95 cents...");
 
         try {
             displaySuccessIfTrue(1 == result.getElement(0));
@@ -103,6 +186,15 @@ public class DynamicChangemakerTestHarness {
             e.printStackTrace();
             displayFailure();
         }
+
+       System.out.println("Testing Swiss Franc Denominations, 2 cents....");
+       result = DynamicChangemaker.makeChangeWithDynamicProgramming(sFDenominations, 2);
+       try {
+           displaySuccessIfTrue(result.isImpossible());
+       } catch (Exception e) {
+           e.printStackTrace();
+           displayFailure();
+       }
     }
 
     public static void test_Euros() {
@@ -110,7 +202,7 @@ public class DynamicChangemakerTestHarness {
 
         Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(euroDenominations, 99);
 
-        System.out.println("Testing Euro Denominations...");
+        System.out.println("Testing Euro Denominations, 99 cents...");
 
         try {
             displaySuccessIfTrue(0 == result.getElement(0));
@@ -153,6 +245,50 @@ public class DynamicChangemakerTestHarness {
             displayFailure();
         }
 
+        result = DynamicChangemaker.makeChangeWithDynamicProgramming(euroDenominations, 5243);
+        System.out.println("Testing Euro Denominations, 5,243 cents...");
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+        try {
+            displaySuccessIfTrue(1 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(104 == result.getElement(5));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
 
     }
 
@@ -160,7 +296,7 @@ public class DynamicChangemakerTestHarness {
         int[] usaDenominations = new int[] { 25, 10, 5, 1 };
 
         Tuple result = DynamicChangemaker.makeChangeWithDynamicProgramming(usaDenominations, 99);
-        System.out.println("Testing USA Denominations...");
+        System.out.println("Testing USA Denominations, 99 cents...");
 
         try {
             displaySuccessIfTrue(3 == result.getElement(0));
@@ -189,6 +325,73 @@ public class DynamicChangemakerTestHarness {
             e.printStackTrace();
             displayFailure();
         }
+
+        System.out.println("Testing USA Denominations, 99 cents (but the denominations are mixed up)...");
+
+        usaDenominations = new int[] { 10, 25, 1, 5 };
+        result = DynamicChangemaker.makeChangeWithDynamicProgramming(usaDenominations, 99);
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(3 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(4 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        usaDenominations = new int[] { 25, 10, 5, 1 };
+        result = DynamicChangemaker.makeChangeWithDynamicProgramming(usaDenominations, 5216);
+        System.out.println("Testing USA Denominations, 5,216 cents...");
+
+        try {
+            displaySuccessIfTrue(208 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+
     }
 
 }
